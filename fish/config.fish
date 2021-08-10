22f31d1727fb9cd0ffde6ee65d63126272481c59
Apprentice76 <hrk.som.01@gmail.com>
2021-08-10 18:28:34 +0530

    Aug 10 18:28

--- fish/config.fish
+++
@@ -1,9 +0,0 @@
-if status is-interactive
-    set fish_greeting
-    source "$HOME/.config/fish/aliases.fish"
-    # Commands to run in interactive sessions can go here
-    # source "$HOME/.config/fish/themes/slavic-cat.fish"
-    # source "$HOME/.config/fish/themes/pie/pie.fish"
-end
-
-# starship init fish | source
